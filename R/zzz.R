2f6d7fb8df7db30059d514987924c6732b0e5a84
C. Regouby <christophe.regouby@free.fr>
2025-03-11 09:04:02 +0100

    wrong place to fix that

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   utils <- loadNamespace('utils')
   getHelpFile <- get('.getHelpFile', envir = utils)
   body(getHelpFile)[[2]] <- substitute({
-    on.exit(return(rhelpi18n:::.translateHelpFile(sub("{\u005C}","{\u005C\u005C}",returnValue()), pkgname, file)))
+    on.exit(return(rhelpi18n:::.translateHelpFile(returnValue(), pkgname, file)))
     step1
   }, list(step1 = body(getHelpFile)[[2]]))
   unlockBinding('.getHelpFile', utils)
